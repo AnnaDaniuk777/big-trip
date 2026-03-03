5008f61b13a47cc35f9554df58f2ab75aaa33bfc
Anna Daniuk <danyukanya57@yandex.ru>
2026-03-03 19:33:42 +0300

    feat: add model and mock data render to presenter and main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import FilterFormView from './view/filter-view/filter-form-view.js';
 import TripInfoView from './view/trip-info-view/trip-info-view.js';
 import TripPresenter from './presenter/trip-presenter.js';
-import { getTripTitle, getTripDates, getTotalCost } from './model/mock/mock-data-generation.js';
+import TripInfoModel from './model/trip-info-model.js';
+import PointsModel from './model/points-model.js';
+import DestinationsModel from './model/destinations-model.js';
+import OffersModel from './model/offers-model.js';
 import { render } from './render.js';
 
 const header = document.querySelector('.page-header');
@@ -10,10 +13,24 @@ const filterContainer = header.querySelector('.trip-controls__filters');
 const sortingContainer = main.querySelector('.trip-events');
 const tripMainContainer = header.querySelector('.trip-main');
 
-const tripPresenter = new TripPresenter(sortingContainer);
+const pointsModel = new PointsModel();
+const destinationsModel = new DestinationsModel();
+const offersModel = new OffersModel();
+const tripInfoModel = new TripInfoModel(pointsModel.getPoints());
+
+const tripPresenter = new TripPresenter(
+  sortingContainer,
+  pointsModel,
+  destinationsModel,
+  offersModel
+);
+
+const title = tripInfoModel.getTripTitle();
+const dates = tripInfoModel.getTripDates();
+const cost = tripInfoModel.getTotalCost();
 
 render(
-  new TripInfoView(getTripTitle(), getTripDates(), getTotalCost()),
+  new TripInfoView(title, dates, cost),
   tripMainContainer,
   'afterbegin'
 );
--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,19 +1,15 @@
 import SortingFormView from '../view/sort-view/sorting-form-view.js';
 import EventCardView from '../view/events-card-view/event-card-view.js';
-// import AddPointFormView from '../view/form-offer-view/form-add-point-view/add-point-form-view.js';
 import EditPointFormView from '../view/form-offer-view/form-edit-point-view/edit-point-form-view.js';
 import EventListView from '../view/events-card-view/event-list-view.js';
 import { render } from '../render.js';
-import {
-  getPoints,
-  getBlankPoint,
-  getDestinationById,
-  getOffersForType
-} from '../model/mock/mock-data-generation.js';
 
 export default class TripPresenter {
-  constructor(tripEventsContainer) {
+  constructor(tripEventsContainer, pointsModel, destinationsModel, offersModel) {
     this.tripEventsContainer = tripEventsContainer;
+    this.pointsModel = pointsModel;
+    this.destinationsModel = destinationsModel;
+    this.offersModel = offersModel;
 
     this.sortingComponent = new SortingFormView();
     this.eventListComponent = new EventListView();
@@ -21,34 +17,30 @@ export default class TripPresenter {
   }
 
   init() {
-    const points = getPoints();
+    const points = this.pointsModel.getPoints();
+
     render(this.sortingComponent, this.tripEventsContainer);
     render(this.eventListComponent, this.tripEventsContainer);
+
     const listElement = this.eventListComponent.getElement();
 
-    if (points.length > 0) {
-      const firstPoint = points[0];
-      const firstDestination = getDestinationById(firstPoint.destination);
-      const firstOffers = getOffersForType(firstPoint.type);
-      const editForm = new EditPointFormView(firstPoint, firstDestination, firstOffers, false);
-      render(editForm, listElement);
+    const firstPoint = points[0];
+    const firstDestination = this.destinationsModel.getDestinationById(firstPoint.destination);
+    const firstOffers = this.offersModel.getOffersByType(firstPoint.type);
+    const editForm = new EditPointFormView(firstPoint, firstDestination, firstOffers, false);
+    render(editForm, listElement);
 
-      for (let i = 1; i < points.length; i++) {
-        const point = points[i];
-        const destination = getDestinationById(point.destination);
-        const offers = getOffersForType(point.type).filter((offer) =>
-          point.offers.includes(offer.id)
-        );
+    for (let i = 1; i < points.length; i++) {
+      const point = points[i];
+      const destination = this.destinationsModel.getDestinationById(point.destination);
+      const offers = this.offersModel.getOffersByType(point.type).filter((offer) =>
+        point.offers.includes(offer.id)
+      );
 
-        const card = new EventCardView(point, destination, offers);
-        this.eventCards.push(card);
+      const card = new EventCardView(point, destination, offers);
+      this.eventCards.push(card);
 
-        render(card, listElement);
-      }
-    } else {
-      const blankPoint = getBlankPoint();
-      const newForm = new EditPointFormView(blankPoint, {}, [], true);
-      render(newForm, listElement);
+      render(card, listElement);
     }
 
     this.setupNewEventButtonHandler();
@@ -62,7 +54,6 @@ export default class TripPresenter {
     }
 
     newEventButton.addEventListener('click', () => {
-      const blankPoint = getBlankPoint();
       const listElement = this.eventListComponent.getElement();
       const existingForm = listElement.querySelector('.event--edit');
 
@@ -70,11 +61,18 @@ export default class TripPresenter {
         existingForm.remove();
       }
 
-      const newForm = new EditPointFormView(blankPoint, {}, [], true);
-
+      const newForm = new EditPointFormView({}, [], true);
       render(newForm, listElement, 'afterbegin');
 
       newEventButton.disabled = true;
     });
+  }
+
+  updatePoints() {
+    this.eventCards = [];
+    const listElement = this.eventListComponent.getElement();
+    listElement.innerHTML = '';
+
+    this.init();
   }
 }
